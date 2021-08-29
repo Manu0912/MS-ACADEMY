592639e050ba70af7192ab425fd67f093ea5596e
Manu0912 <emanuelcacheda@hotmail.com>
2021-08-28 21:12:08 -0300

    first commit

---
+++ AgenciaAutos/AgenciaAutos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AgenciaAutos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
