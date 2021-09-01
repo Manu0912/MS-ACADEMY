8e5be6b637d62198896733abc8093448f720184e
Manu0912 <emanuelcacheda@hotmail.com>
2021-09-01 02:32:38 -0300

    clean test code

--- AgenciaAutos/AgenciaAutos/Program.cs
+++ AgenciaAutos/AgenciaAutos/Program.cs
@@ -6,11 +6,7 @@ namespace AgenciaAutos
     {
         static void Main(string[] args)
         {
-            Brands brand = (Brands)1;
-            Console.WriteLine(brand.ToString());
-            //Car exampleCar = new Car("golf",4,"red",Brands.Chevrolet,Transmition.automatic.);
-            //Console.WriteLine("{0} \n {1} \n {2} \n {3} \n {4}", exampleCar.brand, exampleCar.color, exampleCar.doorsNumber, exampleCar.model, exampleCar.transmition );
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
