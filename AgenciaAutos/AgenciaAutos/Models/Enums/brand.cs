1e0b8080bea6698db765328fb4de4458578561e3
Manu0912 <emanuelcacheda@hotmail.com>
2021-09-02 09:39:16 -0300

    Enum id's added

--- AgenciaAutos/AgenciaAutos/Models/Enums/brand.cs
+++ AgenciaAutos/AgenciaAutos/Models/Enums/brand.cs
@@ -2,12 +2,12 @@ using System;
 
     public enum Brands
     {
-        Fiat,
-        Ford,
-        Chevrolet,
-        Peugeot,
-        Renault,
-        Volkswagen
+        Fiat = 0,
+        Ford = 1,
+        Chevrolet = 2,
+        Peugeot = 3,
+        Renault = 4,
+        Volkswagen = 5
     }
 
 
