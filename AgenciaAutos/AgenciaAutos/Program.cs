c5c71f4937d711c28ec2b83eea0c0fe94419225b
Manu0912 <emanuelcacheda@hotmail.com>
2021-09-09 20:58:28 -0300

    appsettings.json added. Host configurated.

--- AgenciaAutos/AgenciaAutos/Program.cs
+++ AgenciaAutos/AgenciaAutos/Program.cs
@@ -3,27 +3,68 @@ using AgenciaAutos.CRUD;
 using Enums;
 using System;
 using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Hosting;
+using AgenciaAutos.Options;
+using System.Threading.Tasks;
 
 namespace AgenciaAutos
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            using IHost host = CreateHostBuilder(args).Build();
+
+            TestCrud();
+            
+            await host.RunAsync();
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, configuration) =>
+                {
+                    configuration.Sources.Clear();
+
+                    IHostEnvironment env = hostingContext.HostingEnvironment;
+
+                    configuration
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true);
+
+                    IConfigurationRoot configurationRoot = configuration.Build();
+
+                    TransientFaultHandlingOptions options = new();
+                    configurationRoot.GetSection(nameof(TransientFaultHandlingOptions))
+                                     .Bind(options);
+
+                    CRUDOptions crudOptions = new();
+                    configurationRoot.GetSection(nameof(CRUDOptions))
+                                     .Bind(crudOptions);
+
+                    Console.WriteLine($"TransientFaultHandlingOptions.Enabled={options.Enabled}");
+                    Console.WriteLine($"TransientFaultHandlingOptions.AutoRetryDelay={options.AutoRetryDelay}");
+
+                });
+
+        static void TestCrud(){
             //test CRUD function
+
             string brand = Enum.GetName(typeof(Brands), 0);
             string transmition = Enum.GetName(typeof(Transmition), 0);
 
-            Car car = new Car("x", 5, "red", brand, transmition,01);
-            CarCRUD carcrud = new CarCRUD(@"F:\cars.json");
+            Car car = new Car("x", 5, "red", brand, transmition, 01);
+            CarCRUD carcrud = new CarCRUD(CRUDOptions.CarsPATH);
             carcrud.Create(car);
 
-            Car car1 = new Car("x", 5, "red", brand, transmition,02);
-            
+            Car car1 = new Car("x", 5, "red", brand, transmition, 02);
+
             carcrud.Create(car1);
 
-            Car car2 = new Car("x", 5, "red", brand, transmition,03);
-           
+            Car car2 = new Car("x", 5, "red", brand, transmition, 03);
+
             carcrud.Create(car2);
 
             string read = carcrud.ReadFile();
@@ -46,5 +87,8 @@ namespace AgenciaAutos
             string json = JsonSerializer.Serialize(carcrud.Get(2), options);
             Console.WriteLine(json);
         }
+
+        
+
     }
 }
