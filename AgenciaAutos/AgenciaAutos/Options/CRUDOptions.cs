2df7eaae3838974d2f54984c30442e4e9667511b
Manu0912 <emanuelcacheda@hotmail.com>
2021-09-13 09:26:59 -0300

    class Client and Rental

--- AgenciaAutos/AgenciaAutos/Options/CRUDOptions.cs
+++ AgenciaAutos/AgenciaAutos/Options/CRUDOptions.cs
@@ -9,5 +9,7 @@ namespace AgenciaAutos.Options
     class CRUDOptions
     {
         public static string CarsPATH { get; set; }
+        public static string ClientsPATH { get; set; }
+        public static string RentalPATH { get; set; }
     }
 }
