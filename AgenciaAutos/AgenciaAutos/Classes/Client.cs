82ca6eee87d90530efaacc312d7720adc71810e8
Manu0912 <emanuelcacheda@hotmail.com>
2021-09-13 09:35:43 -0300

    client class completed

--- AgenciaAutos/AgenciaAutos/Classes/Client.cs
+++ AgenciaAutos/AgenciaAutos/Classes/Client.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AgenciaAutos
 {
     class Client
     {
+        private int dni { get; set; }
+        private string name { get; set; }
+        private string lastName { get; set; }
+        private string telephoneNumber { get; set; }
+        private string address { get; set; }
+        private string city { get; set; }
+        private string state { get; set; }
+        private int postalCode { get; set; }
+        private DateTime lastModified { get; set; }
+
+        Client(int _dni,string _name, string _lastname, string _telephoneNumber, string _address, string _city, string _state, int _postalCode, DateTime _lastModified)
+        {
+            dni = _dni;
+            name = _name;
+            lastName = _lastname;
+            telephoneNumber = _telephoneNumber;
+            address = _address;
+            city = _city;
+            state = _state;
+            postalCode = _postalCode;
+            lastModified = _lastModified;
+        }
     }
 }
