caf5ef144d869a85aa1d7461981390dc573d7254
Manu0912 <emanuelcacheda@hotmail.com>
2021-09-13 09:46:28 -0300

    Rental class completed

--- AgenciaAutos/AgenciaAutos/Classes/Rental.cs
+++ AgenciaAutos/AgenciaAutos/Classes/Rental.cs
@@ -8,5 +8,17 @@ namespace AgenciaAutos
 {
     class Rental
     {
+        private string rentalDuration { get; set; }
+        private string clientDni { get; set; }
+        private string carRentalId { get; set; }
+        private DateTime returnedDate { get; set; }
+
+        Rental(string _rentalDuration, string _clientDni, string _carRentalId, DateTime _returnedDate)
+        {
+            rentalDuration = _rentalDuration;
+            clientDni = _clientDni;
+            carRentalId = _carRentalId;
+            returnedDate = _returnedDate;
+        }
     }
 }
